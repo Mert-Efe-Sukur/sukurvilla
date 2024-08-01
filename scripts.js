607c8bb7bf5cfe8e51e89f1ac07b5189ec155be8
Mert Efe Şükür <88542815+Mert-Efe-Sukur@users.noreply.github.com>
2024-08-01 18:39:38 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,18 @@
+// Initialize EmailJS with your public key
+emailjs.init("cIcj2daicL2Nro6xf");
+
+// Handle form submission
 document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Mesajınız başarıyla gönderildi!');
+    event.preventDefault(); // Prevent default form submission
+
+    // Send the form data using EmailJS
+    emailjs.sendForm('service_vh7x6h5', 'template_p4gljfr', '#contact-form')
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+            alert('Mesajınız başarıyla gönderildi!');
+            document.getElementById('contact-form').reset(); // Reset the form after successful submission
+        }, function(error) {
+            console.log('FAILED...', error);
+            alert('Mesajınız gönderilemedi. Lütfen tekrar deneyin.');
+        });
 });
